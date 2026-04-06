d5539a502f645262ad27d972ee307e18872be79d
ZhangLei <tbanowl@gmail.com>
2026-04-06 22:17:42 +0800

    refactor: 修改前端框架

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/styles/main.css";
-import "flatpickr/dist/l10n/zh";
-import "flatpickr/dist/flatpickr.min.css";
-
-const app = createApp(App);
-app.mount("#app");
